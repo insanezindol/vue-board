d72999901cfcae731fa5e3353c0d96aca0515144
딘 <jhlee@lunasoft.co.kr>
2019-12-20 16:29:50 +0900

    init vue.js board

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        writer: '이진형1',
+        title: '제목1',
+        content: '내용1'
+    },
+    {
+        writer: '이진형2',
+        title: '제목2',
+        content: '내용2'
+    },
+    {
+        writer: '이진형3',
+        title: '제목3',
+        content: '내용3'
+    },
+]
